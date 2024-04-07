3e23dadcc787eba5ff6e2a36b0a74825929a6aba
Andrey <tuguchev.andrey@gmail.com>
2024-04-08 01:11:28 +0500

    feat: Header Logo IMG priority load + Footer template

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,6 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer(){
 
     return (
-        <></>
+        <footer className="border-t">
+            <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+                <Link href="/">
+                    <Image src="/assets/image/logo.svg" width={128} height={38} alt="Event App Logo" />
+                </Link>
+
+                <p>2024 Events App. All Rights Reserved.</p>
+            </div>
+        </footer>
     )
 }
