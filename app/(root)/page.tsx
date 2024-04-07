438240789186f8922f73f2d9bffcfccb7ccc5805
Andrey <tuguchev.andrey@gmail.com>
2024-04-08 04:33:45 +0500

    feat: MongoDB cached connection + Event and User DB models

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Intrducing Cutting-Edge Events Hosting Platorm!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book your tickets for latest events in your local are or anywhere in the world!</p>
+            <h1 className="h1-bold">Introducing Cutting-Edge Events Hosting Platorm!</h1>
+            <p className="p-regular-20 md:p-regular-24">Book your tickets for latest events in your local area or anywhere in the world!</p>
             <Button asChild className="button w-full sm:w-fit" size="lg">
               <Link href="#events">
                 Explore Now
