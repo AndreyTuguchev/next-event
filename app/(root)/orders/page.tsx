59577d33554f9e0aaa4541786b7e72ff65119651
Andrey Tuguchev <tuguchev.andrey@gmail.com>
2025-10-18 01:52:05 +0500

    feat: use modular UI items for search and category-filter

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,35 +1,33 @@
-import Search  from '@/components/shared/Search'
-import { getOrdersByEvent } from '@/lib/actions/order.actions'
-import { formatDateTime, formatPrice } from '@/lib/utils'
-import { SearchParamProps } from '@/types'
-import { IOrderItem } from '@/lib/database/models/order.model'
-import { auth } from '@clerk/nextjs'
-import { getEventById } from '@/lib/actions/event.action'
-import { redirect  } from 'next/navigation'
+import { getOrdersByEvent } from "@/lib/actions/order.actions";
+import { formatDateTime, formatPrice } from "@/lib/utils";
+import { SearchParamProps } from "@/types";
+import { IOrderItem } from "@/lib/database/models/order.model";
+import { auth } from "@clerk/nextjs";
+import { getEventById } from "@/lib/actions/event.action";
+import { redirect } from "next/navigation";
+import Search from "@/components/ui/search";
 
 const Orders = async ({ searchParams }: SearchParamProps) => {
+  const eventId = (searchParams?.eventId as string) || "";
+  const searchText = (searchParams?.query as string) || "";
 
-  const eventId = (searchParams?.eventId as string) || ''
-  const searchText = (searchParams?.query as string) || ''
-  
   const sessionClaims = auth().sessionClaims;
 
   let userRole = sessionClaims?.userRole as string;
 
-  if ( null != userRole  && userRole.toLowerCase().endsWith('admin')) {
-      userRole = "admin";
-  }else{
-      userRole = "user";
+  if (null != userRole && userRole.toLowerCase().endsWith("admin")) {
+    userRole = "admin";
+  } else {
+    userRole = "user";
   }
 
   const loggedInUserId = sessionClaims?.userId as string;
 
   const currentEventData = await getEventById(eventId);
 
-  if ( loggedInUserId !== currentEventData.organizer._id ) redirect('/');
+  if (loggedInUserId !== currentEventData.organizer._id) redirect("/");
 
-
-  const orders = await getOrdersByEvent({ eventId, searchString: searchText })
+  const orders = await getOrdersByEvent({ eventId, searchString: searchText });
 
   return (
     <>
@@ -46,7 +44,9 @@ const Orders = async ({ searchParams }: SearchParamProps) => {
           <thead>
             <tr className="p-medium-14 border-b text-grey-500">
               <th className="min-w-[250px] py-3 text-left">Order ID</th>
-              <th className="min-w-[200px] flex-1 py-3 pr-4 text-left">Event Title</th>
+              <th className="min-w-[200px] flex-1 py-3 pr-4 text-left">
+                Event Title
+              </th>
               <th className="min-w-[150px] py-3 text-left">Buyer</th>
               <th className="min-w-[100px] py-3 text-left">Created</th>
               <th className="min-w-[100px] py-3 text-right">Amount</th>
@@ -66,9 +66,14 @@ const Orders = async ({ searchParams }: SearchParamProps) => {
                     <tr
                       key={row._id}
                       className="p-regular-14 lg:p-regular-16 border-b "
-                      style={{ boxSizing: 'border-box' }}>
-                      <td className="min-w-[250px] py-4 text-primary-500">{row._id}</td>
-                      <td className="min-w-[200px] flex-1 py-4 pr-4">{row.eventTitle}</td>
+                      style={{ boxSizing: "border-box" }}
+                    >
+                      <td className="min-w-[250px] py-4 text-primary-500">
+                        {row._id}
+                      </td>
+                      <td className="min-w-[200px] flex-1 py-4 pr-4">
+                        {row.eventTitle}
+                      </td>
                       <td className="min-w-[150px] py-4">{row.buyer}</td>
                       <td className="min-w-[100px] py-4">
                         {formatDateTime(row.createdAt).dateTime}
@@ -84,7 +89,7 @@ const Orders = async ({ searchParams }: SearchParamProps) => {
         </table>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
