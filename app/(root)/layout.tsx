f2fd5b24f095c9dbe57bee9b37e1c7ee4c42b0c5
Andrey Tuguchev <tuguchev.andrey@gmail.com>
2025-10-18 01:36:38 +0500

    feat: now use new UI and layout items

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
+import Header from "@/components/layout/header";
 import Footer from "@/components/shared/Footer";
-import Header from "@/components/shared/Header";
 
 export default function RootLayout({
   children,
@@ -7,10 +7,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-screen flex-col" >
-        <Header />
-      <main className="flex-1" >{children}</main>
-        <Footer />
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
     </div>
   );
 }
