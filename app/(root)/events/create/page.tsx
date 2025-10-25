057477ae12941e33f67bbeb4cd02892be5c3e9e5
Andrey Tuguchev <tuguchev.andrey@gmail.com>
2025-10-25 23:31:57 +0500

    chore: file prettified

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,40 +1,45 @@
 import EventForm from "@/components/shared/EventForm";
+
 import { isValidUserAction } from "@/lib/actions/user.actions";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+export default async function CreateEvent() {
+  const sessionClaims = auth().sessionClaims;
 
-export default async function CreateEvent (){
+  let userRole = sessionClaims?.userRole as string;
 
-    const sessionClaims = auth().sessionClaims;
+  if (null != userRole && userRole.toLowerCase().endsWith("admin")) {
+    userRole = "admin";
+  } else {
+    userRole = "user";
+  }
 
-    let userRole = sessionClaims?.userRole as string;
+  const userId = sessionClaims?.userId as string;
 
-    if ( null != userRole  && userRole.toLowerCase().endsWith('admin')) {
-        userRole = "admin";
-    }else{
-        userRole = "user";
-    }
+  if (null == userId) redirect("/sign-in");
 
-    const userId = sessionClaims?.userId as string;
+  const isValidUserActionResult = await isValidUserAction(userId);
 
-    if ( null == userId ) redirect('/sign-in')
+  return (
+    <>
+      <section className="!pt-[130px] bg-primary-50 bg-cover bg-center pb-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Create Event
+        </h3>
+      </section>
 
-    const isValidUserActionResult = await isValidUserAction(userId);
-
-    return (
-        <>
-            <section className="!pt-[130px] bg-primary-50 bg-cover bg-center pb-5 md:py-10">
-                <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
-            </section>
-
-            <div className="wrapper my-8">
-            { isValidUserActionResult?.startsWith("Error") 
-            ? <div className={`pt-[50px] px-5 text-center text-lg text-[#f00] font-semibold`}>{isValidUserActionResult}</div> 
-            : <EventForm userId={userId} userRole={userRole} type="Create" />
-            }
-                
-            </div>
-        </>
-    )
+      <div className="wrapper my-8">
+        {isValidUserActionResult?.startsWith("Error") ? (
+          <div
+            className={`pt-[50px] px-5 text-center text-lg text-[#f00] font-semibold`}
+          >
+            {isValidUserActionResult}
+          </div>
+        ) : (
+          <EventForm userId={userId} userRole={userRole} type="Create" />
+        )}
+      </div>
+    </>
+  );
 }
